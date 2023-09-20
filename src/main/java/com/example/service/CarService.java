834109c16bb3c54125e050eb3fc4d7b66baffe2a
Noboru Shimomura <rmmsuoorbnauoi@gmail.com>
2023-09-20 10:01:01 +0900

    lesson02 課題01

--- src/main/java/com/example/service/CarService.java
+++ src/main/java/com/example/service/CarService.java
@@ -11,12 +11,21 @@ import com.example.mapper.CarMapper;
 @Service
 public class CarService {
 	private final CarMapper carMapper;
-	
+
 	@Autowired
 	public CarService(CarMapper carMapper) {
-		this.carMapper=carMapper;
+		this.carMapper = carMapper;
 	}
-	public List<Car> findAll(){
+
+	public List<Car> findAll() {
 		return this.carMapper.findAll();
+	}
+
+	public void insert(String name, Integer passengers) {
+		Car car = new Car();
+		car.setName(name);
+		car.setPassengers(passengers);
+		this.carMapper.insert(car);
+		System.out.println("自動採番されたID： " + car.getId());
 	}
 }
